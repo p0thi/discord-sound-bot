ac73192d2de5af2f0ea833385653d27cbf91dc56
Pascal Pothmann <pascal.pothmann@gmail.com>
2020-03-26 22:44:35 +0100

    bugfix

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const bot = new Discord.Client();
 console.log("starting for " + process.env.NODE_ENV);
 
 const token = process.env.NODE_ENV === 'production' ?
-    "MjM0Mjc4MDEzMjI1Nzk1NTg1.XleTLg.xst7uw1B7YGfflZz9hsaCAlages" : // dev
-    "MTg1NTQ3Mjc2MTcxNjA4MDY0.Xn0fxg.9vsUG7EGvx8I0NJvm4XFtcdjQLU"; // prod
+"MTg1NTQ3Mjc2MTcxNjA4MDY0.Xn0fxg.9vsUG7EGvx8I0NJvm4XFtcdjQLU" : // prod
+    "MjM0Mjc4MDEzMjI1Nzk1NTg1.XleTLg.xst7uw1B7YGfflZz9hsaCAlages"; // dev
 
 bot.on("ready", () => {
     console.log("Bot is ready");
