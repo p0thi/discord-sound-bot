4f7e014593f8803c4ea5dc8c1a216fee9b16338c
Pascal Pothmann <pascal.pothmann@gmail.com>
2020-03-31 16:24:42 +0200

    filename readded to sound schema

--- src/models/Sound.js
+++ src/models/Sound.js
@@ -6,7 +6,7 @@ const soundSchema = new mongoose.Schema({
     // _id: mongoose.Schema.Types.ObjectId,
     command: { type: String, unique: false, required: true},
     description: { type: String, unique: false, required: true },
-    // filename: { type: String, unique: false , required: false, sparse: false},
+    filename: { type: String, unique: false , required: false, sparse: false},
     file: { type: mongoose.Schema.Types.ObjectId, ref: 'AudioFile', unique: true, required: true},
     guild: { type: mongoose.Schema.Types.ObjectId, ref: 'Guild', unique: false, required: true},
     creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: false, required: true},
