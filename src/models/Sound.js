df488865d0e44e11e0d7d414d11571225d508a7d
Pascal Pothmann <pascal.pothmann@gmail.com>
2020-04-07 18:21:02 +0200

    added timestamps to sounds

--- src/models/Sound.js
+++ src/models/Sound.js
@@ -4,13 +4,13 @@ import User from './User';
 
 const soundSchema = new mongoose.Schema({
     // _id: mongoose.Schema.Types.ObjectId,
-    command: { type: String, unique: false, required: true},
+    command: { type: String, unique: false, required: true },
     description: { type: String, unique: false, required: true },
     // filename: { type: String, unique: false , required: false, sparse: false},
-    file: { type: mongoose.Schema.Types.ObjectId, ref: 'AudioFile', unique: true, required: true},
-    guild: { type: mongoose.Schema.Types.ObjectId, ref: 'Guild', unique: false, required: true},
-    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: false, required: true},
-});
+    file: { type: mongoose.Schema.Types.ObjectId, ref: 'AudioFile', unique: true, required: true },
+    guild: { type: mongoose.Schema.Types.ObjectId, ref: 'Guild', unique: false, required: true },
+    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: false, required: true },
+}, { timestamps: true });
 
 soundSchema.index({ command: 1, guild: 1 }, { unique: true })
 
